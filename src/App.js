94ad611ec9113feae46d0677c6d574818f8494dc
Mike <U10604@rts-365.dk>
2022-05-10 11:13:13 +0200

    update css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import Home from "./Home";
 import Navbar from "./Navbar";
+
+const styles = {
+  content: css`
+    max-width: 600px;
+    margin: 40px auto;
+    padding: 20px;
+  `,
+};
+
 function App() {
   return (
     <div className="App">
-    <Navbar />
-      <div className="content">
+      <Navbar />
+      <div css={styles.content}>
         <Home />
       </div>
     </div>
